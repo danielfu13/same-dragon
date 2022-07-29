a9e16675c97f7f8c3b1d147723f3d60bea106035
danielfu13 <daniel.fu123@hotmail.com>
2022-07-29 11:36:29 -0400

    added apollo server/updated server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const path = require('path');
+// const routes = require('./routes');
+
+//db connect
 const db = require('./config/connection');
-const routes = require('./routes');
+
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
+//import apollo server express
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+//apply apollo server with express app
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +32,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+
+//get all
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
